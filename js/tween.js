62ed7416d283c36ece37828110b6f6d3a5efdf0a
sexfat <bryant6831@gmail.com>
2017-04-22 16:25:13 +0800

    update timelinemax

--- js/tween.js
+++ js/tween.js
@@ -1,9 +1,7 @@
 $(function(){
 //tween section
 
-
 $('button').on('click' , function(){
-
  TweenMax.fromTo('.box1',2,{
      y: -50,
      x: 0,
@@ -31,8 +29,52 @@ $('button').on('click' , function(){
      delay: 2,
      ease : Elastic.easeOut
 
- });
+    });
 
  });
  console.log('tween box2 ok');
+
+var controller = new ScrollMagic.Controller();
+
+
+var wp = new TimelineMax().staggerFromTo('.flow .item', 0.5, {
+        x: -50,
+        opacity: 0
+    }, {
+        x: 0,
+        opacity: 1,
+    }, 0.1);
+
+    var scene = new ScrollMagic.Scene({
+             triggerElement: "#section_01",
+             reverse: true,
+             offset:'300px',
+             // duration: '90%'
+         })
+
+      .setTween(wp)
+      .addIndicators()
+      .addTo(controller);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
